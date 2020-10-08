dd03e79039a7b3ab1f5228ca1c86af1a6928eb91
1like2learn <ndespained@gmail.com>
2020-10-08 12:08:00 -0700

    DFS search of map works, dict of key room_id value = dict of key room_id value direction to get there. Also have a set of nodes to remove until empty.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,7 +27,57 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+"""
+Store Rooms visited in a set. Store paths in queue.
+test_line:
+visited = {0, 1, 2}
+queue = [[0],[0,1],[0,1,2]]
+traversal_path = ['n']
+test_cross:
+visited = {0, 1, 5, 3, 7, 2, 4, 6, 8}
+queue = {[0]/,[0,1]/,[0,5]/,[0,3]/,[0,7]/,[0,1,2]/,[0,5,6]/,[0,3,4]/,[0,7,8]/}
+"""
+worldGraph = {player.current_room.get_id(): {}}
 traversal_path = []
+paths = [[player.current_room]]
+# prevRoom = player.current_room
+visited = []
+"""
+Loop through every path in paths, setting the current room to 
+the latest room in the path. Find all the possible directions
+to go. Append a new path to paths with all the possible new 
+rooms added. This should find the shortest possible path to 
+every node using a BFT.
+"""
+while len(paths) > 0:
+    path = paths.pop(-1)
+    curRoom = path[-1]
+    # lastCommonNode = path[-2]
+    # i = 1
+    # while lastCommonRoom != prevRoom:
+    #     roomExit = traversal_path[-i]
+    #     if roomExit == 'n':
+    #         reverseExit = 's'
+    #     elif roomExit == 's':
+    #         reverseExit = 'n'
+    #     elif roomExit == 'e':
+    #         reverseExit = 'w'
+    #     elif roomExit == 'w':
+    #         reverseExit = 'e'
+
+    for roomExit in curRoom.get_exits():
+        nextRoom = curRoom.get_room_in_direction(roomExit)
+        nextRoomId = nextRoom.get_id()
+        worldGraph[curRoom.get_id()][nextRoomId] = roomExit
+        if nextRoomId not in worldGraph:
+            visited.append(nextRoomId)
+            paths.append(path + [nextRoom])
+            worldGraph[nextRoomId] = {}
+
+print('traversal_path: ', traversal_path)
+print('worldGraph: ', worldGraph)
+print('visited: ', visited)
+
 
 
 
